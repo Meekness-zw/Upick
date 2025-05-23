50b347dc1e27ae1e13bb88287c2c5908a834fa41
Mac <mac@Mac.local>
2025-05-23 16:05:37 +0200

    push

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,6 +8,17 @@ module.exports = {
     priority: 0.7,
     sitemapSize: 7000,
     transform: async (config, path) => {
+      // Add XSL stylesheet to the sitemap
+      if (path === 'sitemap.xml') {
+        return {
+          loc: path,
+          changefreq: config.changefreq,
+          priority: config.priority,
+          lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+          alternateRefs: config.alternateRefs ?? [],
+          xsl: '/sitemap.xsl'
+        }
+      }
       return {
         loc: path,
         changefreq: config.changefreq,
