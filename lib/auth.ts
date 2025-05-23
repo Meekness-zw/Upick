08778655a0538fdc4ce0aa259ddf9adaca503eb3
Mac <mac@Mac.local>
2025-05-23 15:34:27 +0200

    don

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,8 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth"
+import { prisma } from "@/lib/prisma"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { compare } from "bcrypt"
-import prisma from "@/lib/prisma"
+import bcrypt from "bcrypt"
 
 declare module "next-auth" {
   interface Session {
@@ -22,7 +22,7 @@ declare module "next-auth/jwt" {
 }
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as any,
   session: {
     strategy: "jwt",
   },
@@ -47,41 +47,54 @@ export const authOptions: NextAuthOptions = {
           },
         })
 
-        if (!user) {
+        if (!user || !user?.hashedPassword) {
           return null
         }
 
-        const isPasswordValid = await compare(credentials.password, user.password)
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        )
 
-        if (!isPasswordValid) {
+        if (!isCorrectPassword) {
           return null
         }
 
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-        }
+        return user
       },
     }),
   ],
   callbacks: {
     async session({ token, session }) {
-      if (token && session.user) {
+      if (token) {
         session.user.id = token.id
         session.user.name = token.name
         session.user.email = token.email
+        session.user.image = token.picture
       }
+
       return session
     },
     async jwt({ token, user }) {
-      if (user) {
-        return {
-          ...token,
-          id: user.id,
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
         }
+        return token
       }
-      return token
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
     },
   },
 }
